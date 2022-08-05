5496b48e513e66878de20db6a5a18dd6628e4eae
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-05 08:56:35 +0300

    remove os.getcwd()

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import supervisely as sly
 from fastapi import FastAPI, Request, Depends
 
 
-app_root_dir = os.getcwd()  # app sources directory (working directory)
+# app_root_dir = os.getcwd()  # app sources directory (working directory)
 
 app = FastAPI()
 sly_app = sly.app.fastapi.create()
@@ -12,13 +12,8 @@ app.mount("/sly", sly_app)
 templates = sly.app.fastapi.Jinja2Templates(directory="templates")
 # sly.app.fastapi.enable_hot_reload_on_debug(app)
 
-print(123)
-
 
 @app.get("/")
 async def read_index(request: Request):
-    template = templates.TemplateResponse(
-        "index.html",
-        {"request": request},
-    )
+    template = templates.TemplateResponse("index.html", {"request": request})
     return template
