af9ab2cef56bb69d431244ceecaf12769b5d72c0
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-05 10:35:53 +0300

    hot reload works

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import supervisely as sly
+import time
 from fastapi import FastAPI, Request, Depends
 from supervisely.app.fastapi import available_after_shutdown
 
@@ -9,8 +10,10 @@ load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 app = FastAPI()
-app.mount("/sly", sly.app.fastapi.create())
-templates = sly.app.fastapi.Jinja2Templates(directory="templates")
+sly.app.fastapi.init(app)
+
+# app.mount("/sly", sly.app.fastapi.create())
+# templates = sly.app.fastapi.Jinja2Templates(directory="templates")
 # sly.app.fastapi.enable_hot_reload_on_debug(app)
 
 from supervisely.app.widgets import ElementButton, SlyTqdm
@@ -21,4 +24,21 @@ button = ElementButton(text="Start")
 
 @app.get("/")
 async def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return sly.app.fastapi.Jinja2Templates().TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+        },
+    )
+
+
+@button.add_route(app=app, route=ElementButton.Routes.BUTTON_CLICKED)
+def count(
+    state: sly.app.StateJson = Depends(sly.app.StateJson.from_request),
+):
+    total = 100
+    pbar = progress(message="Downloading project...", total=total)
+    for i in range(total):
+        time.sleep(0.1)
+        pbar.update(1)
+    pbar.close()
