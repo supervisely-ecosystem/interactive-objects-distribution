2b631d7308f5e71775aa43464f43b0610e7d2e7a
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-05 08:41:08 +0300

    localhost works

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import supervisely as sly
-from fastapi import FastAPI, Request, Depends, StaticFiles
+from fastapi import FastAPI, Request, Depends
 
 
 app_root_dir = os.getcwd()  # app sources directory (working directory)
@@ -9,9 +9,8 @@ app_root_dir = os.getcwd()  # app sources directory (working directory)
 app = FastAPI()
 sly_app = sly.app.fastapi.create()
 app.mount("/sly", sly_app)
-app.mount("/static", StaticFiles(directory=os.path.join(app_root_dir, "static")))
 templates = sly.app.fastapi.Jinja2Templates(directory="templates")
-sly.app.fastapi.enable_hot_reload_on_debug(app)
+# sly.app.fastapi.enable_hot_reload_on_debug(app)
 
 print(123)
 
