a70e54b97d2495b7ce899a179f1230f97d80e896
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-06 19:37:50 +0300

    start plotly - wip

--- src/main.py
+++ src/main.py
@@ -2,7 +2,12 @@ import os
 from dotenv import load_dotenv
 import supervisely as sly
 import time
-from supervisely.app.fastapi import available_after_shutdown
+import json
+import pandas as pd
+import plotly.express as px
+
+# from supervisely.app.fastapi import available_after_shutdown
+
 
 # storage_image_url - server address join using flag
 # available_after_shutdown hiddend - auto in init
@@ -16,6 +21,15 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 app = sly.Application()
 progress = sly.app.widgets.Progress()
 button = sly.app.widgets.Button(text="Start", icon="zmdi zmdi-play")
+
+# df = pd.DataFrame(dict(x=[1, 3, 2, 4], y=[1, 2, 3, 4]))
+# fig = px.line(df, x="x", y="y", title="Unsorted Input")
+
+df = px.data.stocks()
+fig = px.line(df, x="date", y="GOOG")
+
+data = sly.app.DataJson()
+data["chart"] = json.loads(fig.to_json())
 
 
 @button.click
