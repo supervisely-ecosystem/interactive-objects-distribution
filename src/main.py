f59d73b14d04d00d6fe83f87accc95be090b951b
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-06 19:13:40 +0300

    wip - TODO api init exceptions

--- src/main.py
+++ src/main.py
@@ -2,27 +2,28 @@ import os
 from dotenv import load_dotenv
 import supervisely as sly
 import time
-
-from fastapi import FastAPI, Request, Depends
 from supervisely.app.fastapi import available_after_shutdown
 
 # storage_image_url - server address join using flag
 # available_after_shutdown hiddend - auto in init
 # altair visualizations
-# SlyTqdm -> progress / TQDM???
+# api inti print exception + link to documentation
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 app = sly.Application()
-progress = sly.app.widgets.SlyTqdm()
+progress = sly.app.widgets.Progress()
 button = sly.app.widgets.Button(text="Start", icon="zmdi zmdi-play")
+
+api = sly.Api()
+api = sly.Api.from_env()
 
 
 @button.click
 def count():
-    total = 50
+    total = 20
     with progress(message="Some processing...", total=total) as pbar:
         for i in range(total):
             time.sleep(0.1)
