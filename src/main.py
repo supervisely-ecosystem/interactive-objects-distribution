645248db03a937bafe32b615585e5be811cc64ef
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-08 12:40:01 +0300

    start line chart

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import supervisely as sly
 import time
+import json
 import pandas as pd
 
 # from supervisely.app.fastapi import available_after_shutdown
@@ -11,20 +12,13 @@ import pandas as pd
 # available_after_shutdown hiddend - auto in init
 # put point after click?
 
-
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-
 project_id = int(os.environ["modal.state.slyProjectId"])
 api = sly.Api()
 project = api.project.get_info_by_id(project_id)
-print(project)
-print(project.reference_image_url)
-print(project.image_preview_url)
-
-sly.app.StateJson()["abc"] = {}
 
 app = sly.Application()
 progress = sly.app.widgets.Progress()
@@ -32,13 +26,13 @@ button = sly.app.widgets.Button(text="Start", icon="zmdi zmdi-play")
 project_info = sly.app.widgets.ProjectThumbnail(project)
 
 
-import json
-
 data = sly.app.DataJson()
 data["output1"] = {"a": 123}
 data["example1"] = json.loads(
     '{"series":[{"name":"Desktops","data":[10,41,35,51,49,62,69,91,148]}],"chartOptions":{"chart":{"height":350,"type":"line","zoom":{"enabled":false}},"dataLabels":{"enabled":false},"stroke":{"curve":"straight"},"title":{"text":"Product Trends by Month","align":"left"},"grid":{"row":{"colors":["#f3f3f3","transparent"],"opacity":0.5}},"xaxis":{"categories":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep"]}}}'
 )
+
+# print(json.dumps(data["example1"], indent=4))
 
 data["output2"] = {"a": 123}
 data["example2"] = json.loads(
@@ -47,9 +41,19 @@ data["example2"] = json.loads(
 
 
 @button.click
-def count():
+def calculate_stats():
+    stats = {}
+    # for dataset in api.dataset.get_list(project.id):
+    #     images = api.image.get_list(dataset.id)
+    #     for batch in sly.batched(images):
+    #         image_ids = [image.id for image in batch]
+    #         annotations = api.annotation.download_json_batch(dataset.id, image_ids)
+    #         for ann_json in annotations:
+
     total = 20
     with progress(message="Some processing...", total=total) as pbar:
         for i in range(total):
             time.sleep(0.1)
             pbar.update(1)
+
+    return stats
