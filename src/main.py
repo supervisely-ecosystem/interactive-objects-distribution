0c173d17ac19d118bec171b71a6504de40f6d5c3
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-08 16:26:11 +0300

    apexchart click kwargs does not work

--- src/main.py
+++ src/main.py
@@ -48,21 +48,20 @@ chart = sly.app.widgets.LineChart(
 
 
 @chart.click
-def refresh_images_table():
+def refresh_images_table(serie_index, datapoint_index):
+    print("---> ", serie_index, datapoint_index)
     print("refresh_images_table")
 
 
 @button.click
 def calculate_stats():
-    StateJson()["fdsfd"]
-    stats = {}
     # for dataset in api.dataset.get_list(project.id):
     #     images = api.image.get_list(dataset.id)
     #     for batch in sly.batched(images):
     #         image_ids = [image.id for image in batch]
     #         annotations = api.annotation.download_json_batch(dataset.id, image_ids)
     #         for ann_json in annotations:
-
+    stats = {}
     total = 20
     with progress(message="Some processing...", total=total) as pbar:
         for i in range(total):
