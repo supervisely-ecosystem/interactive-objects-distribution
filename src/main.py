0519581c4caab7347d7a3fb3ca920b4c90e486b0
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-09 17:07:11 +0300

    chart works

--- src/main.py
+++ src/main.py
@@ -11,9 +11,6 @@ from supervisely.app.content import DataJson, StateJson
 
 # TODO:
 # from supervisely.app.fastapi import available_after_shutdown - auto in init
-# table per page change default
-# progress bar - checkbox ok, finished message, hide after success
-# x/y axis title - linechart
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
@@ -36,6 +33,7 @@ chart = sly.app.widgets.LineChart(
     xaxis_type="category",
     xaxis_title="Number of objects",
     yaxis_title="Number of images",
+    height=500,
 )
 
 
@@ -55,54 +53,63 @@ def refresh_images_table(datapoint: sly.app.widgets.LineChart.ClickedDataPoint):
 
 @button.click
 def calculate_stats():
-    # "id", "dataset", "name (click)" , "width", "height", "objects", preview,
+
+    # for name in ["maxim", "denis"]:
+    #     total = 20
+    #     with progress(message=f"Generating '{name}' chart...", total=total) as pbar:
+    #         for i in range(total):
+    #             time.sleep(0.1)
+    #             pbar.update(1)
+    #     x, y = generate_random_chart(random.randint(15, 30))
+    #     chart.add_series(name, x, y)
+
+    # return
 
     # class name -> objects count (x) -> images count (y)
-    chart = defaultdict(lambda: defaultdict(int))
+    stats = defaultdict(lambda: defaultdict(int))
 
     # class name -> objects count (x) -> {image_info, row}
-    tables = defaultdict(lambda: defaultdict(lambda: {"infos": [], "rows": []}))
+    # row -> "id", "dataset", "name (click)" , "width", "height", "objects", preview
+    rows = defaultdict(lambda: defaultdict(lambda: {"infos": [], "rows": []}))
 
-    # tables = defaultdict(lambda: defaultdict(lambda: {"infos": [], "rows": []}))
-    # for obj_class in meta.obj_classes:
-    #     obj_class: sly.ObjClass
-    #     chart[obj_class.name] = defaultdict(lambda: defaultdict(int))
-    #     tables[obj_class.name] = defaultdict(
-    #         lambda: defaultdict(lambda: {"infos": [], "rows": []})
-    #     )
+    max_x = 1
+    with progress(message=f"Processing images...", total=project.items_count) as pbar:
+        for dataset in api.dataset.get_list(project.id):
+            images = api.image.get_list(dataset.id)
+            for batch in sly.batched(images):
+                batch_ids = [image.id for image in batch]
+                annotations = api.annotation.download_json_batch(dataset.id, batch_ids)
+                for image, ann_json in zip(batch, annotations):
+                    ann = sly.Annotation.from_json(ann_json, meta)
+                    counters = defaultdict(int)
+                    for label in ann.labels:
+                        counters[label.obj_class.name] += 1
+                    for obj_class in meta.obj_classes:
+                        obj_class: sly.ObjClass
+                        class_name = obj_class.name
+                        objects_count = counters[class_name]
+                        stats[class_name][objects_count] += 1
+                        max_x = max(max_x, objects_count)
+                        rows[class_name][objects_count]["infos"].append(image)
+                        rows[class_name][objects_count]["rows"].append(
+                            [
+                                image.id,
+                                dataset.name,
+                                image.name,
+                                len(ann.labels),
+                                image.width,
+                                image.height,
+                            ]
+                        )
+                    pbar.update(1)
 
-    for dataset in api.dataset.get_list(project.id):
-        images = api.image.get_list(dataset.id)
-        for batch in sly.batched(images):
-            batch_ids = [image.id for image in batch]
-            annotations = api.annotation.download_json_batch(dataset.id, batch_ids)
-            for image, ann_json in zip(batch, annotations):
-                ann = sly.Annotation.from_json(ann_json, meta)
-                counters = defaultdict(int)
-                for label in ann.labels:
-                    counters[label.obj_class.name] += 1
-                for class_name, objects_count in counters.items():
-                    chart[class_name][objects_count] += 1
-                    tables[class_name][objects_count]["infos"].append(image)
-                    tables[class_name][objects_count]["rows"].append(
-                        [
-                            image.id,
-                            dataset.name,
-                            image.name,
-                            len(ann.labels),
-                            image.width,
-                            image.height,
-                        ]
-                    )
-
-    for name in ["maxim", "denis"]:
-        total = 20
-        with progress(message=f"Generating '{name}' chart...", total=total) as pbar:
-            for i in range(total):
-                time.sleep(0.1)
-                pbar.update(1)
-        x, y = generate_random_chart(random.randint(15, 30))
-        chart.add_series(name, x, y)
+    for class_name, d in stats.items():
+        x = list(range(max_x + 1))
+        y = [0] * len(x)
+        for px, py in d.items():
+            y[px] = py
+        # x, y = list(d.keys()), list(d.values())
+        chart.add_series(class_name, x, y)
 
 
 @table.click
