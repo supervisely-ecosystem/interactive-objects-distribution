44ae50e4d88b2a966d8a88856d3a6246b21db2d4
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-08-10 15:58:22 +0300

    Initialization (#1)
    * wip
    
    * localhost works
    
    * remove os.getcwd()
    
    * remove getcwd
    
    * wip
    
    * remove line
    
    * available_after_shutdown
    
    * available_after_shutdown does not work
    
    * button and progress start
    
    * hot reload works
    
    * refactor
    
    * clean
    
    * SDK sources link
    
    * todos
    
    * todos
    
    * altair todo
    
    * venv todos
    
    * workds
    
    * index.html - wip
    
    * wip - start js versions
    
    * venv script updated
    
    * page title from env APP_NAME
    
    * most of the tasks done
    
    * button loading
    
    * button done
    
    * depends wip
    
    * loglevel debug
    
    * wip
    
    * remove comments
    
    * wip - TODO api init exceptions
    
    * wip
    
    * start plotly - wip
    
    * start plotly
    
    * test line chart
    
    * linechart2
    
    * DD lyne-chart - sort of work, switch to apexcharts
    
    * clean
    
    * apex works
    
    * project thumbnail done
    
    * start line chart
    
    * low level apex done
    
    * apexchart raw - done
    
    * line chart
    
    * apexchart click kwargs does not work
    
    * click works
    
    * first demo with clicks done
    
    * todos
    
    * demo example: +++
    
    * start table
    
    * jinja widgets click if-else wip
    
    * wip
    
    * demo done
    
    * start logic
    
    * progress - hide on finish
    
    * html cleaning - wip denis
    
    * stats calculation work in progress
    
    * chart works
    
    * yaxis_autorescale
    
    * ymax mutation
    
    * gallery - todo
    
    * set_image
    
    * wip
    
    * all widgets
    
    * table columns wip
    
    * wip
    
    * wip- works
    
    * todos
    
    * todo
    
    * stats logic  in separate file
    
    * refactoring
    
    * button hide and show
    
    * done labels
    
    * wip
    
    * clean comments
    
    * wip
    
    * label done
    
    * preview button in table
    
    * emoji
    
    * remove vertical gap
    
    * remove todos
    
    * update config
    
    Co-authored-by: Maxim Kolomeychenko <maxim@supervise.ly>

---
+++ src/main.py
@@ -0,0 +1,74 @@
+import os
+from dotenv import load_dotenv
+import supervisely as sly
+import src.stats as stats
+
+# for convenient debug, has no effect in production
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api = sly.Api()
+app = sly.Application()
+
+# get project info from server
+project_id = int(os.environ["modal.state.slyProjectId"])
+project = api.project.get_info_by_id(project_id)
+meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
+stats.init(project, meta)
+
+
+# initialize widgets we will use in UI
+project_info = sly.app.widgets.ProjectThumbnail(project)
+progress = sly.app.widgets.Progress()
+button = sly.app.widgets.Button(text="Calculate stats", icon="zmdi zmdi-play")
+finish_msg = sly.app.widgets.Text(status="success")
+chart = sly.app.widgets.LineChart(
+    title="Objects count distribution for every class",
+    xaxis_type="category",
+    xaxis_title="Number of objects",
+    yaxis_title="Number of images",
+)
+chart_click_info = sly.app.widgets.NotificationBox()
+table = sly.app.widgets.Table(fixed_cols=1, width="100%")
+labeled_image = sly.app.widgets.LabeledImage()
+
+
+@button.click
+def calculate_stats():
+    with progress(message=f"Processing images...", total=project.items_count) as pbar:
+        for dataset in api.dataset.get_list(project.id):
+            images = api.image.get_list(dataset.id)
+            for batch in sly.batched(images):
+                batch_ids = [image.id for image in batch]
+                annotations = api.annotation.download_json_batch(dataset.id, batch_ids)
+                for image, ann_json in zip(batch, annotations):
+                    ann = sly.Annotation.from_json(ann_json, meta)
+                    stats.increment(dataset, image, ann)
+                    pbar.update(1)
+
+    for class_name, x, y in stats.get_series():
+        chart.add_series(class_name, x, y)
+
+    button.hide()
+    finish_msg.text = "Statistics has been successfully calculated"
+
+
+@chart.click
+def refresh_images_table(datapoint: sly.app.widgets.LineChart.ClickedDataPoint):
+    df = stats.get_table_data(cls_name=datapoint.series_name, obj_count=datapoint.x)
+    table.read_pandas(df)
+    chart_click_info.set(
+        title="Table for clicked chart datapoint",
+        description=f"Images that have at least {datapoint.x} object(s) of class {datapoint.series_name}",
+    )
+
+
+@table.click
+def show_image(datapoint: sly.app.widgets.Table.ClickedDataPoint):
+    labeled_image.loading = True
+    image_id = datapoint.row["id"]
+    image = api.image.get_info_by_id(image_id)
+    ann_json = api.annotation.download_json(image_id)
+    ann = sly.Annotation.from_json(ann_json, meta)
+    labeled_image.set(title=image.name, image_url=image.preview_url, ann=ann)
+    labeled_image.loading = False
---
+++ src/stats.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+import pandas as pd
+import supervisely as sly
+
+
+team: sly.TeamInfo = None
+workspace: sly.WorkspaceInfo = None
+project: sly.ProjectInfo = None
+meta: sly.ProjectMeta = None
+
+table_columns = ["id", "image", "dataset", "labels", "width", "height", "preview"]
+# class name -> objects count (x) -> (images count - y) and (table rows)
+stats = defaultdict(lambda: defaultdict(lambda: {"images_count": 0, "table_rows": []}))
+max_x = 1
+
+
+def init(project_info: sly.ProjectInfo, project_meta: sly.ProjectMeta):
+    api = sly.Api()
+    global team, workspace, project, meta
+
+    project = project_info
+    meta = project_meta
+    if len(meta.obj_classes) == 0:
+        raise ValueError("App finished: project does not have classes")
+
+    workspace = api.workspace.get_info_by_id(project_info.workspace_id)
+    team = api.team.get_info_by_id(workspace.team_id)
+
+
+def increment(
+    dataset: sly.DatasetInfo,
+    image: sly.ImageInfo,
+    ann: sly.Annotation,
+):
+    global max_x
+    counters = defaultdict(int)
+    for label in ann.labels:
+        counters[label.obj_class.name] += 1
+    for obj_class in meta.obj_classes:
+        class_name = obj_class.name
+        objects_count = counters[class_name]
+        stats[class_name][objects_count]["images_count"] += 1
+        max_x = max(max_x, objects_count)
+        labeling_url = sly.image.get_labeling_tool_url(
+            team.id, workspace.id, project.id, dataset.id, image.id
+        )
+
+        stats[class_name][objects_count]["table_rows"].append(
+            [
+                image.id,
+                sly.image.get_labeling_tool_link(labeling_url, image.name),
+                dataset.name,
+                len(ann.labels),
+                image.width,
+                image.height,
+                # sly.utils.get_preview_link(),
+                sly.app.widgets.Table.get_html_text_as_button(),
+            ]
+        )
+
+
+def dict_to_xy(d: dict):
+    x = list(range(max_x + 1))
+    y = [0] * len(x)
+    for px, py in d.items():
+        y[px] = py["images_count"]
+    return x, y
+
+
+def get_series():
+    for class_name, d in stats.items():
+        x, y = dict_to_xy(d)
+        yield class_name, x, y
+
+
+def get_table_data(cls_name, obj_count):
+    rows = stats[cls_name][obj_count]["table_rows"]
+    df = pd.DataFrame(rows, columns=table_columns)
+    return df
