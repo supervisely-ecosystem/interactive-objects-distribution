1805646b572fe3d0bddc59d95beebab2a95a0be2
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-10 11:52:08 +0300

    stats logic  in separate file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
 import os
-from collections import defaultdict
 from dotenv import load_dotenv
 import supervisely as sly
-import pandas as pd
-from src.calc import dict_to_xy, increment_stats
+import src.stats as stats
 
 # TODO:
 # from supervisely.app.fastapi import available_after_shutdown - auto in init
-# table - fixed_cols test
-# table - image name clickable + icon
-# table - preview column
-# line chart - yaxis_autorescale=False
 # grid gallery - empty gallery message
 # label - click on XXX to see YYY
 # label - you clicked XXX and it is YYY
-# lines or bars or confusion matrix???? ??? YY
+# hide button after sucess
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
@@ -27,12 +21,7 @@ app = sly.Application()
 project_id = int(os.environ["modal.state.slyProjectId"])
 project = api.project.get_info_by_id(project_id)
 meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
-if len(meta.obj_classes) == 0:
-    raise ValueError("App finished: project does not have classes")
-table_columns = ["id", "image", "dataset", "labels", "width", "height"]
-tables_rows = None
-stats = None
-
+stats.init(project, meta)
 
 # define all UI widgets here
 project_info = sly.app.widgets.ProjectThumbnail(project)
@@ -43,8 +32,6 @@ chart = sly.app.widgets.LineChart(
     xaxis_type="category",
     xaxis_title="Number of objects",
     yaxis_title="Number of images",
-    yaxis_autorescale=True,
-    height=350,
 )
 table = sly.app.widgets.Table(fixed_cols=1, width="100%")
 labeled_image = sly.app.widgets.LabeledImage()
@@ -52,14 +39,6 @@ labeled_image = sly.app.widgets.LabeledImage()
 
 @button.click
 def calculate_stats():
-    global stats, tables_rows
-    # class name -> objects count (x) -> images count (y)
-    stats = defaultdict(lambda: defaultdict(int))
-
-    # class name -> objects count (x) -> {image_info, row}
-    tables_rows = defaultdict(lambda: defaultdict(lambda: []))
-
-    max_x = 1
     with progress(message=f"Processing images...", total=project.items_count) as pbar:
         for dataset in api.dataset.get_list(project.id):
             images = api.image.get_list(dataset.id)
@@ -68,22 +47,16 @@ def calculate_stats():
                 annotations = api.annotation.download_json_batch(dataset.id, batch_ids)
                 for image, ann_json in zip(batch, annotations):
                     ann = sly.Annotation.from_json(ann_json, meta)
-                    max_x = increment_stats(
-                        stats, tables_rows, max_x, dataset, image, ann, meta
-                    )
+                    stats.increment(dataset, image, ann)
                     pbar.update(1)
 
-    for class_name, d in stats.items():
-        x, y = dict_to_xy(d, max_x)
+    for class_name, x, y in stats.get_series():
         chart.add_series(class_name, x, y)
 
 
 @chart.click
 def refresh_images_table(datapoint: sly.app.widgets.LineChart.ClickedDataPoint):
-    class_name = datapoint.series_name
-    objects_count = datapoint.x
-    rows = tables_rows[class_name][objects_count]
-    df = pd.DataFrame(rows, columns=table_columns)
+    df = stats.get_pandas_table(cls_name=datapoint.series_name, obj_count=datapoint.x)
     table.read_pandas(df)
 
 
