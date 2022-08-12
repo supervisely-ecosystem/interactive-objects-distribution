7593e05cd1edf1a3a5b2261944c6bab954e7a61a
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-12 13:27:34 +0300

    test click error

--- src/main.py
+++ src/main.py
@@ -35,6 +35,13 @@ labeled_image = sly.app.widgets.LabeledImage()
 
 @button.click
 def calculate_stats():
+    # from async_asgi_testclient import TestClient
+    # from supervisely.app.fastapi.utils import run_sync
+    # client = TestClient(app.get_server())
+    # resp = run_sync(client.post("/sly/shutdown"))
+
+    raise ValueError("123")
+
     with progress(message=f"Processing images...", total=project.items_count) as pbar:
         for dataset in api.dataset.get_list(project.id):
             images = api.image.get_list(dataset.id)
